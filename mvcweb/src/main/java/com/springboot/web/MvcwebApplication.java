949128e9fc0ffc2349b23f2aa10e6ae97a47cbd4
chaudharykajal <chaudharykajal456@gmail.com>
2023-03-28 18:34:48 +0530

    SB

---
+++ mvcweb/src/main/java/com/springboot/web/MvcwebApplication.java
@@ -0,0 +1,14 @@
+package com.springboot.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MvcwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcwebApplication.class, args);
+	}
+
+}
