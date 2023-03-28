fdf3501c28a115bf3f162d6d4a0edd20b9aeddd0
chaudharykajal <chaudharykajal456@gmail.com>
2023-03-28 20:31:01 +0530

    changes made to webmvc

--- webMVC/src/main/java/com/webmvc/WebMvcApplication.java
+++ webMVC/src/main/java/com/webmvc/WebMvcApplication.java
@@ -3,12 +3,15 @@ package com.webmvc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
+
 public class WebMvcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebMvcApplication.class, args);
-		System.out.println("A");
+		System.out.println("Test case 1");
 	}
 
 }
--- webMVC/src/main/java/com/webmvc/controller/TestMVC.java
+++ webMVC/src/main/java/com/webmvc/controller/TestMVC.java
@@ -1,15 +1,15 @@
-package com.webmvc.Controller;
+package com.webmvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class TestController {
+public class TestMVC {
+
 	@GetMapping("/index")
-	public ModelAndView getPage()
-	{
-		System.out.println("Test case 1");
+	public ModelAndView getPage() {
+		System.out.println("Test Case 123");
 		return new ModelAndView("index");
 	}
 }
